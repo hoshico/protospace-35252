60daf884d228c5aa2dbeefe07576a55014f570a4
hoshico <hoshimoto-yasuyuki@yasuyukinoMacBook-Air.local>
2021-04-03 23:02:16 +0900

    ユーザー詳細

--- db/migrate/20210402102741_create_comments.rb
+++ db/migrate/20210402102741_create_comments.rb
@@ -3,9 +3,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
     create_table :comments do |t|
       t.references :prototype, foreign_key: true
       t.references :user,      foreign_key: true
-
-      t.text       :text,   null: false
-
+      
+      t.text    :text,   null: false
       t.timestamps
     end
   end
